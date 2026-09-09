7df57cc101cd771f105bceb56c7d630819707a52
Ahmed Fekry <ahmedfikry78@gmail.com>
2026-09-09 15:04:24 +0400

    added the dbcontext and unique on username and email

---
+++ Backend/UserManagement.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+
+            modelBuilder.Entity<Role>().HasIndex(u => u.Name) .IsUnique();
+
+        }
+    }
+}
