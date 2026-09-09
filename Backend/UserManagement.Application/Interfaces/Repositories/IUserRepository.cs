d8cc5772db837dd2156f7acf03c5fb98e685edd8
Ahmed Fekry <ahmedfikry78@gmail.com>
2026-09-09 14:50:46 +0400

    added the respository interfaces

---
+++ Backend/UserManagement.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Application.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task<User> ByIdAsync(int id);
+        public Task AddAsync(User user);
+        public Task UpdateAsync(User user);
+        public Task DeleteAsync(int id);
+        public Task<IEnumerable<User>> GetAllAsync();
+    }
+}
