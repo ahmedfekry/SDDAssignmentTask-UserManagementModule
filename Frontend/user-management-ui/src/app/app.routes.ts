201b5867668a257fb3f53797179e1a586f7354d4
Ahmed Fekry <ahmedfikry78@gmail.com>
2026-09-12 16:13:04 +0400

    added the routes

--- Frontend/user-management-ui/src/app/app.routes.ts
+++ Frontend/user-management-ui/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import("./home/home").then((m) => m.Home);
+    }
+  },
+  {
+    path: 'signin',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import("./auth/signin/signin").then((m) => m.Signin);
+    }
+  }
+];
--- Frontend/user-management-ui/src/app/app.ts
+++ Frontend/user-management-ui/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Signin } from './componenets/auth/signin/signin';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Signin],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
