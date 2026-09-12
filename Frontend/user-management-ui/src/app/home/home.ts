d000b758c0c3f195757d20b6f2e01fa08412c479
Ahmed Fekry <ahmedfikry78@gmail.com>
2026-09-12 16:22:27 +0400

    added the sidebar and the header

--- Frontend/user-management-ui/src/app/home/home.ts
+++ Frontend/user-management-ui/src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../components/sidebar/sidebar';
+import { Header } from '../components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Sidebar, Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
